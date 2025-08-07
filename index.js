0527bddca39664263b816879e16385ae81b263fc
Nagaprajwal B <136093225+NagaprajwalB@users.noreply.github.com>
2025-08-07 17:49:01 +0530

    updates

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const http = require('http');
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, Node.js!');
-});
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
+app.get('/', (req, res) => res.send('Hello from CI/CD pipeline!'));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
